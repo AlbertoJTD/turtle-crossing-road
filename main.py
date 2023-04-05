a919744691b41a4a35b92f022f5b07f9eb7f739e
AlbertoJTD <albertojtd@outlook.com>
2023-04-05 13:36:27 -0600

    Add game over message

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     # Detect successful crossing
     if player.ycor() > 270:
--- scoreboard.py
+++ scoreboard.py
@@ -1,8 +1,5 @@
 from turtle import Turtle
 
-FONT = ("Courier", 24, "normal")
-
-
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
@@ -20,3 +17,7 @@ class Scoreboard(Turtle):
     def nex_level(self):
         self.level += 1
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align='center', font=('Courier', 20, 'bold'))
