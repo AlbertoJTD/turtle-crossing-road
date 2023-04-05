2094e2dec3bd8ecbcd0025c6466c32e4d91207b4
AlbertoJTD <albertojtd@outlook.com>
2023-04-05 12:51:51 -0600

    Create cars randomly

--- car_manager.py
+++ car_manager.py
@@ -8,14 +8,21 @@ MOVE_INCREMENT = 10
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
-        self.create_car()
+        self.all_cars = []
+        self.hideturtle()
 
     def create_car(self):
-        self.penup()
-        self.shapesize(stretch_wid=2, stretch_len=1)
-        self.color(random.choice(COLORS))
-        self.goto()
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle('square')
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
 
-    def reset_position(self):
-        pass
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
 
--- main.py
+++ main.py
@@ -9,7 +9,9 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+# Object instances
 player = Player()
+car_manager = CarManager()
 scoreboard = Scoreboard()
 
 # player controls
@@ -22,6 +24,8 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    car_manager.create_car()
+    car_manager.move_cars()
     if player.ycor() > 270:
         print('Next level')
 
